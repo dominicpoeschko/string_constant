0efe3c7b3961eb5ec6fbad9788eb6d1d64c83514
Patrick Hanner <hannerpatrick@gmail.com>
2023-02-13 16:56:20 +0100

    Added documentation for project

---
+++ examples/compile_time_fmt.cpp
@@ -0,0 +1,12 @@
+#include "string_constant/string_constant.hpp"
+#include <fmt/format.h>
+#include <iostream>
+
+
+int main(){
+    using namespace sc::literals;
+    
+    static constexpr auto compileString = SC_FORMAT("CompileTime {}", 32);
+    static_assert(compileString == "CompileTime 32"_sc);
+    return 0;
+}
